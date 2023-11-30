db26547a9decc8031fb144e684115ec118a15d4c
Winkermann <willmeschke@gmail.com>
2023-11-30 12:43:11 -0300

    exercicio 03

--- exercicio-03/src/App.tsx
+++ exercicio-03/src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import Promotion from "./routes/Home/Promotion";
@@ -10,7 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route index element={<HomeBody />} />
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<HomeBody />} />
           <Route path="promotion" element={<Promotion />} />
           <Route path="subscription" element={<Subscription />} />
         </Route>
--- exercicio-03/src/components/Header/Header.tsx
+++ exercicio-03/src/components/Header/Header.tsx
@@ -1,14 +1,43 @@
 import { Link } from "react-router-dom";
 import "./styles.css";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <>
       <header>
-        <div className="container">
-          <Link to="/">
-            <h1 className="site-title">MeuSite</h1>
-          </Link>
+        <div className="container navbar-container">
+          <div>
+            <Link to="/">
+              <h1 className="site-title">MeuSite</h1>
+            </Link>
+          </div>
+          <nav className="navbar">
+            <NavLink
+              to="/home"
+              className={({ isActive }) =>
+                isActive ? "navbar-item navbar-link-active" : "navbar-item"
+              }
+            >
+              Início
+            </NavLink>
+            <NavLink
+              to="/promotion"
+              className={({ isActive }) =>
+                isActive ? "navbar-item navbar-link-active" : "navbar-item"
+              }
+            >
+              Promoção
+            </NavLink>
+            <NavLink
+              to="/subscription"
+              className={({ isActive }) =>
+                isActive ? "navbar-item navbar-link-active" : "navbar-item"
+              }
+            >
+              Participar
+            </NavLink>
+          </nav>
         </div>
       </header>
     </>
